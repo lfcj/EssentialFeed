ae317afd1ea834a6b014e9251ac7f80a7dbcc1d2
lfcj <fernanda16400@gmail.com>
2021-10-13 07:37:42 +0200

    Test number of load calls

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,25 +6,37 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestURLs.isEmpty)
     }
 
     func test_load_requestsDataFromURL() {
         let (sut, client) = makeSUT(url: URL(string: "https://www.another-url.com")!)
         sut.load()
         XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestURLs.count, 1)
+    }
+
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let (sut, client) = makeSUT(url: URL(string: "https://www.another-url.com")!)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestURLs.count, 2)
+        XCTAssertNotNil(client.requestedURL)
     }
 
     // MARK: - Helpers
 
-    class HTTPClientMock: HTTPClient {
-        private(set) var requestedURL: URL?
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        var requestURLs = [URL]()
 
-        func get(from url: URL?) {
+        func get(from url: URL) {
+            self.requestURLs.append(url)
             self.requestedURL = url
         }
     }
 
-    func makeSUT(url: URL = URL(string: "https://www.google.com")!, client: HTTPClientMock = HTTPClientMock()) -> (RemoteFeedLoader, HTTPClientMock) {
+    private func makeSUT(url: URL = URL(string: "https://www.google.com")!, client: HTTPClientSpy = HTTPClientSpy()) -> (RemoteFeedLoader, HTTPClientSpy) {
         (RemoteFeedLoader(url: url, client: client), client)
     }
 

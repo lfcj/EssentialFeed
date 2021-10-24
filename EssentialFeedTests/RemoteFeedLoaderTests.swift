4d93f4b0536f5e6edefe0476c4e6b8ff39242364
lfcj <fernanda16400@gmail.com>
2021-10-24 17:06:21 +0200

    3. Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths
    Delivers connectivity error on client error

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -24,13 +24,28 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNotNil(client.requestedURL)
     }
 
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in capturedError = error}
+
+        XCTAssertEqual(capturedError, .connectivity)
+        
+    }
+
     // MARK: - Helpers
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
         var requestURLs = [URL]()
+        var error: Error?
 
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             self.requestURLs.append(url)
             self.requestedURL = url
         }

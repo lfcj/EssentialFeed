a68290f35df95b0fa6dee9bef5be33871437516d
lfcj <fernanda16400@gmail.com>
2021-10-24 17:13:06 +0200

    3. Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths
    Replace stubbing by capturing values instead (spying)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -26,10 +26,12 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
 
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
 
         XCTAssertEqual(capturedErrors, [.connectivity])
         
@@ -40,12 +42,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
         var requestURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
 
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             self.requestURLs.append(url)
             self.requestedURL = url
         }

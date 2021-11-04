19a07dfee561ad7370d22bb064dfeb6b61ec25a2
lfcj <fernanda16400@gmail.com>
2021-11-04 18:56:49 +0100

    Make RemoteFeedLoader conform to FeedLoader protocol

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ public protocol HTTPClient {
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
-public final class RemoteFeedLoader {
+public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
 

b0c18642183b90e78e4657289225c260e08d5bc7
Luisa Castaño <lcastano@kayak.com>
2022-01-28 22:08:28 +0100

    Add URLSessionHttpClient to its own file and create target for CI tests

---
+++ EssentialFeed/Feed API/URLSessionHttpClient.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public class URLSessionHttpClient: HTTPClient {
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    struct UnexpectedValuesRepresentation: Error {}
+
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }
+        dataTask.resume()
+    }
+}
+

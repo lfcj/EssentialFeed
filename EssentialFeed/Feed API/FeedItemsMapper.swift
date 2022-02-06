30c9d0fa3d2c03249e8ca8928cad130d5a531bd2
lfcj <fernanda16400@gmail.com>
2022-02-06 21:50:27 +0100

    Add `RemoteFeedItem` data transfer representation to decouple the items mapper from `FeedItem` data models

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -3,32 +3,17 @@ import Foundation
 final class FeedItemsMapper {
 
     private struct FeedItemResult: Decodable {
-        let items: [Item]
-        var feed: [FeedItem] {
-            items.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-
-        var item: FeedItem {
-            FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        let items: [RemoteFeedItem]
     }
 
     static var OK_200: Int { 200 }
 
-    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200, let result = try? JSONDecoder().decode(FeedItemResult.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
 
-        return .success(result.feed)
+        return result.items
     }
 
 }
-
--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -24,10 +24,26 @@ public final class RemoteFeedLoader: FeedLoader {
             // The RemoteFeedLoader maps client errors to domain errors.
             switch result {
             case let .success(data, response):
-                completion(FeedItemsMapper.map(data, from: response))
+                completion(RemoteFeedLoader.map(data: data, from: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
         }
+    }
+
+    private static func map(data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+
+}
+
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        map { FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) }
     }
 }

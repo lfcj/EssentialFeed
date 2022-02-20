63b98be2cc84e5ef5c32122d20c4b9e0ea497bea
Luisa Castaño <lcastano@kayak.com>
2022-02-20 20:43:23 +0100

    Move `FeedCachePolicy` to its own file and make it an `enum` so it has no initializers and because it is a value type

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+enum FeedCachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+    private static let maxCacheAgeInDays = 7
+
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}

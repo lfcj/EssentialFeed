668534f4e50a723a3eb245ccd3b85dbf845eb28e
lfcj <fernanda16400@gmail.com>
2022-02-28 21:21:03 +0100

    Nest `LoadFeedResult` into `FeedLoader` protocol as `FeedLoader.Result` since they are closely related

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-public typealias LoadFeedResult = Result<[FeedImage], Error>
-
 public protocol FeedLoader {
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+    typealias Result = Swift.Result<[FeedImage], Error>
+    func load(completion: @escaping (FeedLoader.Result) -> Void)
 }

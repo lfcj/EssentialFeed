53f7a50c4574e07d5b26485bee30139befd33736
lfcj <fernanda16400@gmail.com>
2022-03-02 22:22:59 +0100

    Remove comments to make a dummy comment and trigger new actions

--- EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -9,13 +9,9 @@ import XCTest
 
 class EssentialFeediOSTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
     func testExample() throws {
         // This is an example of a functional test case.

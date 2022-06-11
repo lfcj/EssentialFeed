9fde49583daad663bfc1ecf0d66ca2735c586a53
Luisa Castaño <lcastano@kayak.com>
2022-06-11 20:08:51 +0200

    Extract cache feed image data use case into the new `CacheFeedImageDataUseCaseTests` test case class

---
+++ EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -0,0 +1,9 @@
+//
+//  CacheFeedImageDataUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Luisa Fernanda Castaño on 11.06.22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation

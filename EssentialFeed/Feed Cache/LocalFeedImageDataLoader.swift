7c53c9a46bebc9dce6bd5f880a557223d9fcdec3
Luisa Castaño <lcastano@kayak.com>
2022-06-11 19:19:35 +0200

    Move `FeedImageDataStore` to production

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -1,10 +1,5 @@
 import Foundation
 
-public protocol FeedImageDataStore {
-    typealias Result = Swift.Result<Data?, Error>
-    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
-}
-
 public final class LocalFeedImageDataLoader: FeedImageDataLoader {
 
     public enum Error: Swift.Error {

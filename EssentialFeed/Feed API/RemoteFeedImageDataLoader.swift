3eaa5942c6a789b61e62de535ca5abd44e323143
Luisa Castaño <lcastano@kayak.com>
2022-06-11 09:34:18 +0200

    Extract `RemoteFeedImageDataLoader` into production file

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -1,1 +1,57 @@
+import Foundation
 
+public final class RemoteFeedImageDataLoader {
+
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)? = nil
+        var wrapped: HTTPClientTask?
+
+        init(completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+
+        func cancel() {
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+
+    private let client: HTTPClient
+
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion: completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else {
+                return
+            }
+            switch result {
+            case let .success((data, response)):
+                if data.isEmpty || response.statusCode != 200 {
+                    task.complete(with: .failure(Error.invalidData))
+                } else {
+                    task.complete(with: .success(data))
+                }
+            case .failure(let error):
+                task.complete(with: .failure(error))
+            }
+        }
+        return task
+    }
+}
--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -1,62 +1,7 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedImageDataLoader {
-
-    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)? = nil
-        var wrapped: HTTPClientTask?
-
-        init(completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-
-    private let client: HTTPClient
-    init(client: HTTPClient) {
-        self.client = client
-    }
-
-    enum Error: Swift.Error {
-        case invalidData
-    }
-
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPClientTaskWrapper(completion: completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else {
-                return
-            }
-            switch result {
-            case let .success((data, response)):
-                if data.isEmpty || response.statusCode != 200 {
-                    task.complete(with: .failure(Error.invalidData))
-                } else {
-                    task.complete(with: .success(data))
-                }
-            case .failure(let error):
-                task.complete(with: .failure(error))
-            }
-        }
-        return task
-    }
-}
-
-class RemoteFeedImageDataLoaderTests: XCTestCase {
+final class RemoteFeedImageDataLoaderTests: XCTestCase {
 
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()

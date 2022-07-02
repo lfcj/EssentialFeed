5fd88e757e88393c8ddb97041ad15a86df2e9ff7
Luisa Castaño <lcastano@kayak.com>
2022-07-02 08:55:56 +0200

    Test `FeedPresenter` sends message to start loading

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -1,21 +1,61 @@
 import XCTest
 
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+protocol FeedLoadingView: AnyObject {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
 final class FeedPresenter {
 
-    init(view: Any) {}
+    private let loadingView: FeedLoadingView
+
+    init(loadingView: FeedLoadingView) {
+        self.loadingView = loadingView
+    }
+
+    func didStartLoadingFeed() {
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
 
 }
 
 final class FeedPresenterTests: XCTestCase {
 
     func test_feedPresenter_doesNotSendAnythingOnInitialisation() {
-        let view = ViewSpy()
-        _ = FeedPresenter(view: view)
+        let (_, view) = makeSUT()
         XCTAssertTrue(view.messages.isEmpty, "Expected no messages")
     }
 
-    private class ViewSpy {
-        private(set) var messages: [Any] = []
+    func test_feedPresenter_sendsMessageWhenItStartsLoadingFeed() {
+        let (presenter, view) = makeSUT()
+
+        presenter.didStartLoadingFeed()
+        XCTAssertEqual(view.messages, [.display(isLoading: true)])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedPresenter, ViewSpy) {
+        let view = ViewSpy()
+        let presenter = FeedPresenter(loadingView: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(presenter, file: file, line: line)
+        return (presenter, view)
+    }
+
+    private class ViewSpy: FeedLoadingView {
+        enum Message: Equatable {
+            case display(isLoading: Bool)
+        }
+
+        private(set) var messages: [Message] = []
+
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.append(.display(isLoading: viewModel.isLoading))
+        }
+        
     }
 
 }

3ec86f89191d38fc303b4e8a7c20812f3c7348d7
lfcj <fernanda16400@gmail.com>
2022-07-18 09:58:11 +0200

    Move `FeedCache` to prouction `Feed Feature` group

---
+++ EssentialFeed/Feed Feature/FeedCache.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public protocol FeedCache {
+    typealias SaveResult = Result<Void, Error>
+    typealias SaveCompletion = (SaveResult) -> Void
+    func save(_ feed: [FeedImage], completion: @escaping SaveCompletion)
+}

08896f9d563fd14ba3384442a1aa9d219968eacf
Luisa Castano <lcastano@kayak.com>
2022-10-30 22:16:43 +0100

    Set FeedEndpoint limit to 10

--- EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/Feed API/FeedEndpoint.swift
@@ -4,6 +4,13 @@ public enum FeedEndpoint {
     case get
 
     public func url(baseURL: URL) -> URL {
-        baseURL.appendingPathComponent("/v1/feed")
+        var components = URLComponents()
+        components.scheme = baseURL.scheme
+        components.host = baseURL.host
+        components.path = baseURL.path + "/v1/feed"
+        components.queryItems = [
+            URLQueryItem(name: "limit", value: "10")
+        ]
+        return components.url!
     }
 }
